d9c0d2e427370d4efdebb2829dca38d0b9fa2505
wrublebowski <jakwroblewski@gmail.com>
2020-06-08 22:54:18 +0200

    Bootstrap for base.html and home.html

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -5,7 +5,7 @@ class PortfolioProject(models.Model):
 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image1 = models.ImageField(upload_to='portfolio/pictures/')
+    image = models.ImageField(upload_to='portfolio/pictures/')
     # provide the direction     and pip install pillow
 
     url = models.URLField(blank=True)
